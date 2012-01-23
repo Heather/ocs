4b1eddc103dedd2d497192f7bb8ede9e1d859f1d
nCdy <X@nCdy.org>
2012-01-23 09:09:12 +0400

    ocsake remake, no make.scm anymore

--- make.scm
+++
@@ -1,4 +0,0 @@
-;;TODO: Platform detecting feature
-;;(if (= system-type 'gnu/linux) ) 
-(system "make")
-(system "pause")
--- tools/make.ml
+++ tools/make.ml
@@ -2,8 +2,25 @@ open Printf
 open Ocs_env
 open Ocs_types
 
+let print_sval v =
+  let port = Ocs_port.output_port Pervasives.stdout in
+  Ocs_print.print port false v;
+  Ocs_port.flush port;
+  print_endline "\n"
 let _ =
   let env = Ocs_top.make_env () in
   let thread = Ocs_top.make_thread () in
+  let handler v = print_sval v in
+  let code =
+    let sval = (Ocs_read.read_from_string "
+		;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+		(system \"make\")
+		(system \"pause\")
+		;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+		") in
+    print_endline "create-dynmic-code:";
+    print_sval sval;
+    Ocs_compile.compile env sval
+  in
   print_endline "Runing make...";
-  Ocs_prim.load_file env thread "make.scm"
+  Ocs_eval.eval thread handler code
